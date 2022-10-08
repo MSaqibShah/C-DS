46829d2b05981476f8890e372816e972af3e474f
MSaqibShah <msaqibshah001@gmail.com>
2022-10-08 23:48:02 +0530

    single linked list

---
+++ SingleLL.c
@@ -0,0 +1,283 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node *flink;
+};
+
+typedef struct node* NODE;
+
+// Function prototypes
+NODE createNode();
+NODE createSingleList();
+NODE appendToSingleList();
+NODE insertIntoSingleList();
+void displayList(NODE HEAD);
+int lengthOfList(NODE HEAD);
+// void updateDataInList(NODE HEAD, int POS);
+int getNodeByPos(NODE HEAD, int POS,NODE *currentNode, NODE *previousNode);
+NODE updateDataInList(NODE HEAD, int POS, int data);
+// =================== CREATE =================== 
+// Create a node 
+NODE createNode(){
+    NODE newNode;
+    int data;
+    newNode = (NODE)malloc(sizeof(NODE));
+
+    printf("\nEnter the Data for the node: ");
+
+    scanf("%d", &data);
+    newNode -> data = data;
+    newNode->flink = NULL;
+    printf("\nNode Created Succesfully!");
+    return newNode;
+}
+
+
+// Create a List
+NODE createSingleList(){
+    printf("\n++++++++++++++++++++++++++++++++");
+    printf("\nCreating a singly linked list...");
+    // printf("\n++++++++++++++++++++++++++++++++");
+    
+    int N;
+    NODE head, newNode, next;
+    
+    printf("\nEnter number of nodes in the list: ");
+    scanf("%d", &N);
+    
+    if(N == 0){
+        return NULL;
+    }
+    
+    
+    head = createNode();
+    next = head;
+    for(int i = 0; i<N-1; i++){
+        newNode = createNode();
+        next -> flink = newNode;
+        next = next-> flink;
+    }
+    printf("\nList Created Succesfully!");
+    displayList(head);
+    printf("\n++++++++++++++++++++++++++++++++");
+    printf("\n");
+    
+    return head;
+}
+
+// =================== READ ===================
+void displayList(NODE HEAD){
+    NODE temp = HEAD;
+    printf("\n");
+    // printf("=======================================\n");
+    while(temp->flink !=NULL){
+      printf("|%d| -> ", temp-> data);
+      temp = temp->flink;
+  }
+  printf("|%d|\n", temp->data);
+//   printf("=======================================\n");
+  return;
+}
+
+// =================== UPDATE ===================
+
+// Append a Node to a list
+NODE appendToSingleList(NODE HEAD){
+    printf("\nAppending...\n");
+    
+    NODE temp = HEAD, newNode;
+    newNode = createNode();
+    while(temp->flink!=NULL){
+        temp= temp->flink;
+    }
+    temp->flink = newNode;
+    
+    printf("Node Appended Succesfully!\n");
+    displayList(HEAD);
+    return HEAD;
+    
+}
+
+// Insert a node anywhere on a list i.e front, last, or End
+NODE insertIntoSingleList(NODE HEAD, int POS)
+{
+    printf("\n++++++++++++++++++++++++++++++++");
+    printf("\nTrying to insert...\n");
+    int len = lengthOfList(HEAD);
+    NODE currentNode = HEAD, previousNode = HEAD, newNode;
+    newNode = createNode();
+    int status = getNodeByPos(HEAD, POS, &currentNode, &previousNode);
+
+    if (status==200||202){
+        previousNode->flink = newNode;
+        newNode->flink = currentNode;
+        printf("\nNode Inserted Succesfully!");
+        displayList(HEAD);
+    }else if(status==201){
+        newNode->flink = currentNode;
+        HEAD = newNode;
+        printf("\nNode Inserted Succesfully!");
+        displayList(HEAD);
+    }
+    else{
+        printf("Insertion Failed");
+    }
+printf("\n++++++++++++++++++++++++++++++++");
+printf("\n");
+return HEAD;
+}
+
+NODE updateDataInList(NODE HEAD, int POS, int data){
+    printf("\n++++++++++++++++++++++++++++++++");
+    printf("\nTrying to update data...\n");
+    // int len = lengthOfList(HEAD);
+    NODE currentNode = HEAD, previousNode = HEAD, newNode;
+    int status = getNodeByPos(HEAD, POS, &currentNode, &previousNode);
+
+    if (status==200 || status==201){
+        // POS == 1
+        currentNode->data = data;
+        printf("\nNode Updated Succesfully!");
+        displayList(HEAD);
+    }else if(status==202){
+        printf("\nERROR: %d is not a valid position as the list contains only %d elements", POS, POS-1);
+        printf("\nERROR: Update Failed");
+    }
+    else{
+        printf("\nERROR: Update Failed");
+    }
+printf("\n++++++++++++++++++++++++++++++++");
+printf("\n");
+return HEAD;
+}
+
+// =================== DELETE ===================
+NODE deleteNodeFromList(NODE HEAD, int POS){
+    printf("\n++++++++++++++++++++++++++++++++");
+    printf("\nTrying to delete node...\n");
+    // int len = lengthOfList(HEAD);
+    NODE currentNode = HEAD, previousNode = HEAD, deletedNode;
+    int status = getNodeByPos(HEAD, POS, &currentNode, &previousNode);
+
+    if (status==200){
+        deletedNode =currentNode;
+        previousNode-> flink = currentNode->flink;
+        printf("Deleted %d", deletedNode->data);
+        printf("\nNode Deleted Succesfully!");
+        deletedNode->flink = NULL;
+        free(deletedNode);
+        displayList(HEAD);
+    }else if(status == 201){
+        deletedNode =currentNode;
+        HEAD= HEAD->flink;
+        printf("Deleted %d", deletedNode->data);
+        printf("\nNode Deleted Succesfully!");
+        deletedNode->flink = NULL;
+        free(deletedNode);
+        displayList(HEAD); 
+    }
+    else if(status==202){
+        printf("\nERROR: %d is not a valid position as the list contains only %d elements", POS, POS-1);
+        printf("\nERROR: Delete Failed");
+    }
+    else{
+        printf("\nERROR: Delete Failed");
+    }
+printf("\n++++++++++++++++++++++++++++++++");
+printf("\n");
+return HEAD;
+}
+
+
+
+// Auxallay Functions
+int lengthOfList(NODE HEAD){
+    NODE temp=HEAD;
+    int length = 0;
+
+    while(temp!=NULL){
+        length++;
+        temp = temp->flink ;
+    }
+    return length;
+}
+
+int getNodeByPos(NODE HEAD, int POS, NODE* currentNode, NODE* previousNode)
+{
+    int  status, len;
+    *currentNode = *previousNode = HEAD;
+    len = lengthOfList(HEAD);
+    
+    if (HEAD == NULL)
+    {
+        // List is Empty
+        printf("\nERROR: List is Empty\n");
+        status = 401;
+        return status;
+    }
+    else if (POS < 1)
+    {
+        printf("\nERROR: POS cannot be less than 1\n");
+        printf("\t\tValue of POS: %d\n", POS);
+        status = 100;
+        return status;
+    }
+    else if (POS > len && POS != len+1)
+    {
+        printf("\nERROR: %d is not a valid position as the list contains only %d elements", POS, len);
+        status = 402;
+        return status;
+    }
+    else
+    {
+        if (POS == 1)
+        {
+            *previousNode = NULL;
+            status=201;
+            return status;
+        }
+        else if(POS==len+1){
+            int i = 1;
+            while(i<POS){;
+                *previousNode = *currentNode;
+                *currentNode = (*currentNode)->flink;
+                i++;
+            }
+            status = 202;
+            return status;
+        }
+        else
+        {
+            int i = 1;
+            while(i<POS){;
+                *previousNode = *currentNode;
+                *currentNode = (*currentNode)->flink;
+                i++;
+            }
+            status = 200;
+            return status;
+        }
+    }
+}
+
+int main() {
+    
+  NODE singleList = createSingleList();
+    int len = lengthOfList(singleList);
+//   singleList = appendToSingleList(singleList);
+
+
+    //  singleList = insertIntoSingleList(singleList, 3);
+    // singleList = updateDataInList(singleList, 3, 1000);
+    singleList = deleteNodeFromList(singleList, 3);
+    
+     
+}
+
+
+
+
+
